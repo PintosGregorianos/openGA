53a9a854c915e2fb3204d901d8327a61aef4679f
Grégory Gusberti <gregorygusberti@hotmail.com>
2014-10-14 18:04:19 -0300

    polySpace + matlabPlot
    Added polySpace (still not tested)
    Added matlabPlot (appears to works)

---
+++ randomSpace/polySpace.cpp
@@ -0,0 +1,45 @@
+#include "polySpace.hpp"
+
+void polySpace::randomizePoly(void) {
+    for (auto &d : my_coeffs) {
+        for (auto &x : d) {
+            x = 2.f*real_rand() - 1.f;
+        }
+    }
+}
+
+polySpace::polySpace(std::size_t dimensions,std::size_t size,std::size_t order) {
+    n_coeffs = order;
+    resize(dimensions,size);
+    my_coeffs.resize(dimensions);
+    for (auto &x : my_coeffs) x.resize(n_coeffs);
+
+    randomizePoly();
+    generate();
+}
+
+void polySpace::setOrder(std::size_t order) {
+    n_coeffs = order;
+}
+
+const float *polySpace::getDimensionCoefficients(std::size_t dimension) const {
+    return &my_coeffs[dimensions].front();
+}
+
+float polySpace::getCoefficient(std::size_t dimension,std::size_t coefficient) const {
+    return my_coeffs[dimension][coefficient];
+}
+
+void polySpace::generate(void) {
+    const float step = 2.f / (float)size;
+    for (std::size_t d = 0; d < dimensions; d++) {
+        float x = -1.f;
+        for (std::size_t i = 0; i < size; i++,x += step) {
+            float temp = 0.f;
+            for (std::size_t c = 0; c < n_coeffs; c++) {
+                temp += my_coeffs[d][c]*pow(x,c);
+            }
+            my_space[d][i] = temp;
+        }
+    }
+}
--- randomSpace/polySpace.hpp
+++ randomSpace/polySpace.hpp
@@ -1,35 +1,29 @@
+#pragma once
+
 #include <vector>
+#include <cmath>
 
 #include "randomSpace.hpp"
-//#include "rand.hpp"
+#include "rand.hpp"
+
+//O coment‡rio entre > Ž extremamente necess‡rio!
+using coeffs = std::vector<std::vector<float>/**/>;
 
 class polySpace : public randomSpace {
     private:
-        std::size_t n_polys;
-        std::vector<float>polys;
+        std::size_t n_coeffs;
+        coeffs my_coeffs;
+
+        void randomizePoly(void);
 
     public:
-        polySpace(std::size_t dimensions,std::size_t size,std::size_t order) {
-            n_polys = order;
-            resize(dimensions,size);
-        }
+        polySpace(std::size_t dimensions,std::size_t size,std::size_t order);
 
-        void setOrder(std::size_t order) {
-            n_polys = order;
-        }
+        void setOrder(std::size_t order);
 
-        const float *getCoefficients(void) const {
-            return &polys.front();
-        }
+        const float *getDimensionCoefficients(std::size_t dimension) const;
 
-        float getCoefficient(std::size_t coefficient) const {
-            return polys[coefficient];
-        }
+        float getCoefficient(std::size_t dimension,std::size_t coefficient) const;
 
-        void generate(void) {
-
-        }
-
-
-
+        void generate(void);
 };
